8177155bf61617b3c0f7a61d7003f5ae8e328de6
kai <ekaizhao@outlook.com>
2026-07-06 21:03:36 +0800

    增加环岛入口调试打印

--- code/image.cpp
+++ code/image.cpp
@@ -1086,6 +1086,14 @@ void Search_Border_OTSU(uint8 imageInput[LCDH][LCDW], uint8 Row, uint8 Col, uint
 // 左环岛判断
 void Element_Judgment_Left_Rings()
 {
+    printf("[RINGDBG][L] check RL=%d LL=%d OFF=%d WL=%d L57=%c L58=%c flag=%d\r\n",
+           ImageStatus.Right_Line,
+           ImageStatus.Left_Line,
+           ImageStatus.OFFLine,
+           ImageStatus.WhiteLine,
+           ImageDeal[57].IsLeftFind,
+           ImageDeal[58].IsLeftFind,
+           ImageFlag.image_element_rings_flag);
     // printf("Left_Line:%d,Right_Line:%d\n", ImageStatus.Left_Line, ImageStatus.Right_Line);
     //     Disf = 0;
     if (ImageStatus.Right_Line > 4 || ImageStatus.Left_Line < 16 // 13
@@ -1109,6 +1117,9 @@ void Element_Judgment_Left_Rings()
         if (ImageDeal[Ysite].LeftBoundary_First - ImageDeal[Ysite - 1].LeftBoundary_First > 3)
         {
             Left_RingsFlag_Point1_Ysite = Ysite;
+            printf("[RINGDBG][L] point1_y=%d diff=%d\r\n",
+                   Left_RingsFlag_Point1_Ysite,
+                   ImageDeal[Ysite].LeftBoundary_First - ImageDeal[Ysite - 1].LeftBoundary_First);
             printf("左侧点1的Y值:%d\n", Left_RingsFlag_Point1_Ysite);
             break;
         }
@@ -1118,6 +1129,9 @@ void Element_Judgment_Left_Rings()
         if (ImageDeal[Ysite + 1].LeftBoundary - ImageDeal[Ysite].LeftBoundary > 3)
         {
             Left_RingsFlag_Point2_Ysite = Ysite;
+            printf("[RINGDBG][L] point2_y=%d diff=%d\r\n",
+                   Left_RingsFlag_Point2_Ysite,
+                   ImageDeal[Ysite + 1].LeftBoundary - ImageDeal[Ysite].LeftBoundary);
             printf("左侧点2的Y值:%d\n", Left_RingsFlag_Point2_Ysite);
             break;
         }
@@ -1137,6 +1151,7 @@ void Element_Judgment_Left_Rings()
         {
 
             Ring_Help_Flag = 1;
+            printf("[RINGDBG][L] corner_y=%d\r\n", Ysite);
             printf("左侧上角点Y值:%d\n", Ysite);
             break;
         }
@@ -1149,8 +1164,16 @@ void Element_Judgment_Left_Rings()
     if (Left_RingsFlag_Point2_Ysite > Left_RingsFlag_Point1_Ysite + 1 && Ring_Help_Flag == 0)
     {
         if (ImageStatus.Left_Line > 5) // 13
+        {
             Ring_Help_Flag = 1;
+            printf("[RINGDBG][L] help_by_left_line LL=%d\r\n", ImageStatus.Left_Line);
+        }
     }
+    printf("[RINGDBG][L] summary p1=%d p2=%d help=%d flag=%d\r\n",
+           Left_RingsFlag_Point1_Ysite,
+           Left_RingsFlag_Point2_Ysite,
+           Ring_Help_Flag,
+           ImageFlag.image_element_rings_flag);
     if (Left_RingsFlag_Point2_Ysite > Left_RingsFlag_Point1_Ysite + 1 && Ring_Help_Flag == 1 &&
         ImageFlag.image_element_rings_flag == 0)
     {
@@ -1160,6 +1183,9 @@ void Element_Judgment_Left_Rings()
         ImageFlag.ring_big_small = 1;
 
         ImageStatus.Road_type = LeftCirque;
+        printf("[RINGDBG][L] ENTER left ring p1=%d p2=%d\r\n",
+               Left_RingsFlag_Point1_Ysite,
+               Left_RingsFlag_Point2_Ysite);
         // gpio_set_level(P20_8, 0);
         // wireless_uart_send_byte(9);
     }
@@ -1176,6 +1202,15 @@ void Element_Judgment_Left_Rings()
 //--------------------------------------------------------------
 void Element_Judgment_Right_Rings()
 {
+    printf("[RINGDBG][R] check LL=%d RL=%d OFF=%d WL=%d R56=%c R57=%c R58=%c flag=%d\r\n",
+           ImageStatus.Left_Line,
+           ImageStatus.Right_Line,
+           ImageStatus.OFFLine,
+           ImageStatus.WhiteLine,
+           ImageDeal[56].IsRightFind,
+           ImageDeal[57].IsRightFind,
+           ImageDeal[58].IsRightFind,
+           ImageFlag.image_element_rings_flag);
     if (ImageStatus.Left_Line > 4 || ImageStatus.Right_Line < 16 // 13
         || ImageStatus.OFFLine > 4
         // || Straight_Judge(1, 20, 45) > 1
@@ -1196,6 +1231,9 @@ void Element_Judgment_Right_Rings()
         if (ImageDeal[Ysite - 1].RightBoundary_First - ImageDeal[Ysite].RightBoundary_First > 3)
         {
             Right_RingsFlag_Point1_Ysite = Ysite;
+            printf("[RINGDBG][R] point1_y=%d diff=%d\r\n",
+                   Right_RingsFlag_Point1_Ysite,
+                   ImageDeal[Ysite - 1].RightBoundary_First - ImageDeal[Ysite].RightBoundary_First);
             break;
         }
     }
@@ -1204,6 +1242,9 @@ void Element_Judgment_Right_Rings()
         if (ImageDeal[Ysite].RightBoundary - ImageDeal[Ysite + 1].RightBoundary > 3)
         {
             Right_RingsFlag_Point2_Ysite = Ysite;
+            printf("[RINGDBG][R] point2_y=%d diff=%d\r\n",
+                   Right_RingsFlag_Point2_Ysite,
+                   ImageDeal[Ysite].RightBoundary - ImageDeal[Ysite + 1].RightBoundary);
             break;
         }
     }
@@ -1220,14 +1261,23 @@ void Element_Judgment_Right_Rings()
             ImageDeal[Ysite + 3].RightBorder < ImageDeal[Ysite + 1].RightBorder)
         {
             Ring_Help_Flag = 1;
+            printf("[RINGDBG][R] corner_y=%d\r\n", Ysite);
             break;
         }
     }
     if (Right_RingsFlag_Point2_Ysite > Right_RingsFlag_Point1_Ysite + 1 && Ring_Help_Flag == 0)
     {
         if (ImageStatus.Right_Line > 5)
+        {
             Ring_Help_Flag = 1;
+            printf("[RINGDBG][R] help_by_right_line RL=%d\r\n", ImageStatus.Right_Line);
+        }
     }
+    printf("[RINGDBG][R] summary p1=%d p2=%d help=%d flag=%d\r\n",
+           Right_RingsFlag_Point1_Ysite,
+           Right_RingsFlag_Point2_Ysite,
+           Ring_Help_Flag,
+           ImageFlag.image_element_rings_flag);
     if (Right_RingsFlag_Point2_Ysite > Right_RingsFlag_Point1_Ysite + 1 && Ring_Help_Flag == 1 &&
         ImageFlag.image_element_rings_flag == 0)
     {
@@ -1237,6 +1287,9 @@ void Element_Judgment_Right_Rings()
         ImageFlag.ring_big_small = 1; // 小环
         SystemData.Stop = 1;
         ImageStatus.Road_type = RightCirque;
+        printf("[RINGDBG][R] ENTER right ring p1=%d p2=%d\r\n",
+               Right_RingsFlag_Point1_Ysite,
+               Right_RingsFlag_Point2_Ysite);
         //        flag_ceshi++;
         //        gpio_set_level(Bee1p, 1);
     }
